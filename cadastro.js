9ce19aeaf3e5db60f3192dca5e1b6d1cedf15744
devMachadoA <amachadoamaral2@gmail.com>
2023-04-14 17:52:11 +0100

    adicionei funcionalidades

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,9 @@
 function signUp(email){
     return email
 }
-
 //pega o email e cadastra na plataforma
+
+function mostraMensagem(){
+    return "Bem-vindo ao sistema"
+}
+//adicionei funcao de msg
